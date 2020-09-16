83c5583cfc59d5c0a59fe0852c6e6a25bb4a30ab
Vadim <davydovvip@gmail.com>
2020-09-17 01:03:04 +0500

    First commit

---
+++ Факультет Java/Java 1/javaFirst/src/main/java/org/example/lesson1/SecondExercise.java
@@ -0,0 +1,13 @@
+package org.example.lesson1;
+
+//Создать переменные всех пройденных типов данных и инициализировать их значения.
+
+public class SecondExercise {
+    byte b = -128;
+    short s = -32768;
+    long l = 922337203685477580L;
+    float f = 0.23f;
+    double d = -123.123d;
+    char c = '\u2240';
+    boolean bo = true;
+}
